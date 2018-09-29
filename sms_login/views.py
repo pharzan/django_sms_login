df471336fb749f7e32fff2cb716f7e4bd8e32638
pharzan <farzan.tinati@gmail.com>
2018-09-30 00:03:00 +0330

    added post endpoint for phone number

--- sms_login/views.py
+++ sms_login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from django.views import View
-
+import json
 
 
 class Create(View):
@@ -12,4 +12,7 @@ class Create(View):
 
     def post(self, request):
         # post logic
-        return JsonResponse({'POST': 'true'})
+        json_data = json.loads(request.body) # convert post incoming data to json
+        if('phone_number' in json_data):
+            return JsonResponse({'POST': 'true','status':200})
+        return JsonResponse({'status': 200, 'messge': 'need phone number'})
